efcefc3cb8db442b1aebad9524dac2b70393d7f7
TruongVuMinhVan <sieumc1990@gmail.com>
2024-10-26 23:25:24 +0700

    update

---
+++ DO_AN_WINFORM/DO_AN_WINDOW/SQL/DANH_MUC.cs
@@ -0,0 +1,27 @@
+namespace WindowsFormsApp1.SQL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class DANH_MUC
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public DANH_MUC()
+        {
+            SAN_PHAM = new HashSet<SAN_PHAM>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string dm_id { get; set; }
+
+        [StringLength(50)]
+        public string dm_ten { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SAN_PHAM> SAN_PHAM { get; set; }
+    }
+}
---
+++ DO_AN_WINFORM/DO_AN_WINDOW/Xuat_Hoa_Don.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Xuat_Hoa_Don : Form
+    {
+        public Xuat_Hoa_Don()
+        {
+            InitializeComponent();
+        }
+
+        private void Xuat_Hoa_Don_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
